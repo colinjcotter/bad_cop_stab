f23e8c2206ee2a9552679b1718ec898d36bd2a77
Colin Cotter <colin.cotter@imperial.ac.uk>
2023-04-19 17:20:09 +0100

    fixed kappa factor

--- bad_cop_stab.py
+++ bad_cop_stab.py
@@ -1,6 +1,7 @@
 from firedrake import *
+from firedrake.petsc import PETSc
 
-n = 10
+n = 1
 mesh = UnitSquareMesh(n, n)
 
 degree = 1
@@ -19,26 +20,25 @@ v, q, vhat, qhat = TestFunctions(W)
 
 J = as_tensor([[0, -1], [0, 1]])
 
-def both(u):
-    return u('+') + u('-')
+def jump(u, n):
+    return dot(u('+'), n('+')) + dot(u('-'), n('-'))
 
 n = FacetNormal(mesh)
 
 a = (
     inner(v, kappa*dot(u, J))*dx - inner(div(v), p)*dx 
-    + inner(both(dot(v, n)), phat)*dS
-    - inner(dot(v,n), dot(u,n))*ds
+    + inner(jump(v, n), phat('+'))*dS + inner(dot(v,n), dot(u,n))*ds
     - inner(dot(v('+'), n('+')), dot(u('+'), n('+')) - uhat('+'))/eta*dS
     - inner(dot(v('-'), n('-')), dot(u('-'), n('-')) + uhat('+'))/eta*dS
-    - inner(dot(v, n), dot(u, n) - uhat('+'))/eta*ds
-    + inner(q, dot(p, J) +  div(u))*dx
-    + inner(qhat('+'), both(dot(u, n)))*dS - inner(qhat('+'), phat('+'))*ds
+    - inner(dot(v, n), dot(u, n) - uhat)/eta*ds
+    - inner(q, kappa*dot(p, J) +  div(u))*dx
+    + inner(qhat('+'), jump(u, n))*dS - inner(qhat, phat)*ds
     + inner(vhat('+'), dot(u('+'), n('+')) - uhat('+'))/eta*dS
     + inner(-vhat('+'), dot(u('-'), n('-')) + uhat('+'))/eta*dS
-    + inner(vhat('+'), dot(u, n) - uhat('+'))/eta*ds
+    + inner(vhat, dot(u, n) - uhat)/eta*ds
     )
 
-F = inner(f, q)*dS
+F = inner(f, q)*dx
 
 params = {
     'ksp_type': 'preonly',
@@ -47,5 +47,23 @@ params = {
     'pc_factor_mat_solver_type': 'mumps'
 }
 
+def chop(A, tol=1E-10):
+    # remove (near) zeros from sparsity pattern
+    A.chop(tol)
+    B = PETSc.Mat().create(comm=A.comm)
+    B.setType(A.getType())
+    B.setSizes(A.getSizes())
+    B.setBlockSize(A.getBlockSize())
+    B.setUp()
+    B.setOption(PETSc.Mat.Option.IGNORE_ZERO_ENTRIES, True)
+    B.setPreallocationCSR(A.getValuesCSR())
+    B.assemble()
+    A.destroy()
+    return B
+
+A = assemble(a)
+Amat = chop(A.petscmat)
+Amat.view()
+
 w = Function(W)
-solve(a==F, w, solver_parameters = params)
+#solve(a==F, w, solver_parameters = params)
