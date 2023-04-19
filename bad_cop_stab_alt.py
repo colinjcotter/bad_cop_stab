22883bd2fcb6d1e5d4ad6b1ede7e120f1c822376
Pablo Brubeck <brubeck@protonmail.com>
2023-04-19 18:05:08 +0100

    alternative formulation

---
+++ bad_cop_stab_alt.py
@@ -0,0 +1,56 @@
+from firedrake import *
+from firedrake.petsc import PETSc
+
+nx = 1
+mesh = UnitSquareMesh(nx, nx, reorder=False, distribution_parameters={"partition": False})
+
+degree = 1
+V = VectorFunctionSpace(mesh, "RT", degree, dim=2)
+Q = VectorFunctionSpace(mesh, "DG", degree-1, dim=2)
+T = VectorFunctionSpace(mesh, "HDiv Trace", degree-1, dim=2)
+W = V * Q * T * T
+
+u, p, uhat, phat = TrialFunctions(W)
+v, q, vhat, qhat = TestFunctions(W)
+
+
+eta = Constant(1.0)
+kappa = Constant(1.0)
+ikappa = kappa * Constant([[0, -1], [1, 0]])
+
+n = FacetNormal(mesh)
+u_n = dot(u, n)
+v_n = dot(v, n)
+
+# diagonal terms
+a1 = inner(v, dot(u, ikappa)) * dx - (inner(v_n('+'), u_n('+')) + inner(v_n('-'), u_n('-'))) * dS - inner(v_n, u_n) * ds
+a2 = -inner(q, dot(p, ikappa)) * dx
+a3 = -inner(vhat('+') * (1/eta), uhat('+')) * dS - inner(vhat * (1/eta), uhat) * ds
+a4 = inner(qhat, eta * phat) * ds
+
+# off-diagonal terms
+b = lambda q, u: -inner(q, div(u)) * dx
+c = lambda vhat, u_n: inner(vhat('+') * (1/eta), u_n('+') - u_n('-')) * dS + inner(vhat * (1/eta), u_n) * ds
+d = lambda qhat, u_n: inner(qhat('+'), u_n('+') + u_n('-')) * dS + inner(qhat, u_n) * ds
+
+a = a1 + a2 + a3 + a4 + b(q, u) + b(p, v) + c(vhat, u_n) + c(uhat, v_n) + d(qhat, u_n) + d(phat, v_n)
+
+if False:
+    # damping term
+    du = u - uhat
+    dv = v - vhat
+    a += (inner(dv('+'), eta * du('+')) + inner(dv('-'), eta * du('-'))) * dS + inner(dv, eta * du) * ds
+
+
+bcs = []
+
+A = assemble(a)
+
+PETSc.Sys.Print(tuple(V.dim() for V in W))
+
+from firedrake.preconditioners.hypre_ams import chop
+Amat = chop(A.petscmat)
+Amat.view()
+
+
+solve(a, z, L, bcs=bcs)
