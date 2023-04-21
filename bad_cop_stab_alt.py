767f73271bd0db1d9ce36637fdbdb3a650f76a30
Pablo Brubeck <brubeck@protonmail.com>
2023-04-21 17:09:33 +0100

    Group constraint terms, use TensorFunctionSpace for Trace variables

--- bad_cop_stab_alt.py
+++ bad_cop_stab_alt.py
@@ -5,18 +5,16 @@ nx = 10
 mesh = UnitSquareMesh(nx, nx, reorder=False, distribution_parameters={"partition": False})
 
 degree = 1
-element = BrokenElement(FiniteElement("RT", cell=mesh.ufl_cell(), degree=degree))
-V = VectorFunctionSpace(mesh, element, dim=2)
+RT = FiniteElement("RT", cell=mesh.ufl_cell(), degree=degree)
+V = VectorFunctionSpace(mesh, BrokenElement(RT), dim=2)
 Q = VectorFunctionSpace(mesh, "DG", degree-1, dim=2)
-T = VectorFunctionSpace(mesh, "HDiv Trace", degree-1, dim=4)
+T = TensorFunctionSpace(mesh, "HDiv Trace", degree-1, shape=(2, 2))
 W = V * Q * T
 
 u, p, up_hat = TrialFunctions(W)
 v, q, vq_hat = TestFunctions(W)
-uhat = as_vector([up_hat[0], up_hat[1]])
-phat = as_vector([up_hat[2], up_hat[3]])
-vhat = as_vector([vq_hat[0], vq_hat[1]])
-qhat = as_vector([vq_hat[2], vq_hat[3]])
+uhat, phat = up_hat[0], up_hat[1]
+vhat, qhat = vq_hat[0], vq_hat[1]
 
 eta = Constant(1.0)
 kappa = Constant(1.0)
@@ -26,26 +24,26 @@ n = FacetNormal(mesh)
 u_n = dot(u, n)
 v_n = dot(v, n)
 
+both = lambda f: f('+') + f('-')
 
 # diagonal terms
-a1 = inner(v, dot(ikappa, u)) * dx - (inner(v_n('+') * (1/eta), u_n('+')) + inner(v_n('-') * (1/eta), u_n('-'))) * dS - inner(v_n * (1/eta), u_n) * ds
+a1 = inner(v, dot(ikappa, u)) * dx - both(inner(v_n / eta, u_n)) * dS - inner(v_n * (1/eta), u_n) * ds
 a2 = -inner(q, dot(ikappa, p)) * dx
 a3 = -inner(vhat('+') * (1/eta), uhat('+')) * dS - inner(vhat * (1/eta), uhat) * ds
 a4 = inner(qhat, eta * phat) * ds
 
-
 # off-diagonal terms
 b = lambda q, u: -inner(q, div(u)) * dx
-c = lambda vhat, u_n: (inner(vhat('+') * (1/eta), u_n('+') - u_n('-'))) * dS + inner(vhat * (1/eta), u_n) * ds
-d = lambda qhat, u_n: (inner(qhat('+'), u_n('+') + u_n('-'))) * dS + inner(qhat, u_n) * ds
-
-a = a1 + a2 + a3 + a4 + b(q, u) + b(p, v) + c(vhat, u_n) + c(uhat, v_n) + d(qhat, u_n) + d(phat, v_n)
+c = lambda vhat, qhat, u_n: ( (inner(-vhat('-') / eta + qhat('-'), u_n('-'))
+                              + inner(vhat('+') / eta + qhat('+'), u_n('+'))) * dS
+                             + inner(vhat / eta + qhat, u_n) * ds)
+a = a1 + a2 + a3 + a4 + b(q, u) + b(p, v) + c(vhat, qhat, u_n) + c(uhat, phat, v_n)
 
 if False:
     # damping term
     du = u - uhat
     dv = v - vhat
-    a += (inner(dv('+'), eta * du('+')) + inner(dv('-'), eta * du('-'))) * dS + inner(dv, eta * du) * ds
+    a += both(inner(dv, eta * du)) * dS + inner(dv, eta * du) * ds
 
 
 f = Constant([1,0])
@@ -68,7 +66,6 @@ cparams = {
     "ksp_type": "gmres",
     "pc_type": "python",
     "pc_python_type": "firedrake.ASMStarPC",
-    "pc_star_mat_ordering_type": "metisnd",
     "pc_star_construct_dim": 0,
     "pc_star_sub_sub": factor, # the first sub is PCASM and second is subsolver
 }
