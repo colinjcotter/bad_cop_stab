4e228d2116779cc12f32d2e8fcbaab08bd4c5ff3
Pablo Brubeck <brubeck@protonmail.com>
2023-04-22 14:18:59 +0100

    use Crouzeix--Raviart instead of HDiv Trace

--- bad_cop_stab_alt.py
+++ bad_cop_stab_alt.py
@@ -6,68 +6,97 @@ import argparse
 parser = argparse.ArgumentParser(description='Stabilised conforming Hdiv badcop')
 parser.add_argument('--kappa', type=float, default=1.0, help='kappa')
 parser.add_argument('--eta', type=float, default=1.0, help='eta')
+parser.add_argument('--cr', action="store_true", default=False, help='use Crosieux--Raviart instead of HDiv Trace')
 parser.add_argument('--damp', action="store_true", default=False, help='damp high frequencies')
+parser.add_argument('--complex', action="store_true", default=False, help='use complex mode')
+parser.add_argument('--quadrilateral', action="store_true", default=False, help='use tensor-product cells')
 parser.add_argument('--nx', type=int, default=10, help='elements along each side')
 parser.add_argument('--degree', type=int, default=1, help='degree')
 args, _ = parser.parse_known_args()
 
+complex_mode = args.complex
+dist_params = {"overlap_type": (DistributedMeshOverlapType.VERTEX, 1),}
+
 nx = args.nx
-mesh = UnitSquareMesh(nx, nx)
+mesh = UnitSquareMesh(nx, nx, quadrilateral=args.quadrilateral, distribution_parameters=dist_params)
 
 degree = args.degree
-RT = FiniteElement("RT", cell=mesh.ufl_cell(), degree=degree)
-V = VectorFunctionSpace(mesh, BrokenElement(RT), dim=2)
-Q = VectorFunctionSpace(mesh, "DG", degree-1, dim=2)
-T = TensorFunctionSpace(mesh, "HDiv Trace", degree-1, shape=(2, 2))
-W = V * Q * T
+cell = mesh.ufl_cell()
+is_simplex = cell.is_simplex()
 
+RT = FiniteElement("RT" if is_simplex else "RTCF", cell=cell, degree=degree)
+DG = FiniteElement("DG" if is_simplex else "DQ", cell=cell, degree=degree-1)
+if args.cr:
+    HDivT = FiniteElement("CR", cell=cell, degree=degree)
+else:
+    HDivT = FiniteElement("HDiv Trace", cell=cell, degree=degree-1)
+
+BrokenRT = BrokenElement(RT)
+if complex_mode:
+    j = Constant(1j)
+    T = VectorElement(HDivT, dim=2)
+else:
+    j = Constant([[0, -1], [1, 0]])
+    BrokenRT = VectorElement(BrokenRT, dim=2)
+    DG = VectorElement(DG, dim=2)
+    T = TensorElement(HDivT, shape=(2, 2))
+
+W = FunctionSpace(mesh, MixedElement([BrokenRT, DG, T]))
 u, p, up_hat = TrialFunctions(W)
 v, q, vq_hat = TestFunctions(W)
-uhat, phat = up_hat[0], up_hat[1]
-vhat, qhat = vq_hat[0], vq_hat[1]
-
-eta = Constant(args.eta)
-kappa = Constant(args.kappa)
-ikappa = kappa * Constant([[0, -1], [1, 0]])
 
 n = FacetNormal(mesh)
 u_n = dot(u, n)
 v_n = dot(v, n)
 
+uhat, phat = up_hat[0], up_hat[1]
+vhat, qhat = vq_hat[0], vq_hat[1]
+
+eta = Constant(args.eta)
+kappa = Constant(args.kappa)
+ikappa = kappa * j
+inv_eta = 1 / eta
+
+def minus(expr):
+    return expr('-')
+
+def plus(expr):
+    return expr('+')
+
 def both(expr):
     return expr('+') + expr('-')
 
 # usual diagonal terms
-a = (inner(v, dot(ikappa, u)) * dx
-     - inner(q, dot(ikappa, p)) * dx
-     + inner(qhat, eta * phat) * ds)
+a = (- inner(v, ikappa * u) * dx
+     + inner(q, ikappa * p) * dx
+     - inner(qhat, eta * phat) * ds)
 
 # usual divergence off-diagonal terms
-b = lambda q, u: -inner(q, div(u)) * dx
-a += b(q, u) + b(p, v)
+a += (inner(q, div(u)) + inner(div(v), p)) * dx
 
 # constraint off-diagonal terms (with shifted Lagrange multiplier)
-c = lambda vhat, qhat, u_n: ( (inner(-vhat('-') + qhat('-'), u_n('-'))
-                               +inner(vhat('+') + qhat('+'), u_n('+')) ) * dS
-                             + inner(vhat / eta + qhat, u_n) * ds)
-a += c(vhat, qhat, u_n) + c(uhat, phat, v_n)
+c = lambda vhat, qhat, u_n: ((minus(inner(qhat - vhat, u_n))
+                              + plus(inner(qhat + vhat, u_n))) * dS
+                             + inner(qhat + inv_eta * vhat, u_n) * ds)
+
+a -= c(vhat, qhat, u_n) + c(uhat, phat, v_n)
 
 # stabilization diagonal terms
-a -= (inner(vhat('+'), uhat('+')) * dS + inner(vhat / eta, uhat) * ds
-      + both(inner(v_n, u_n)) * dS + inner(v_n / eta, u_n) * ds)
+a += both(inner(v_n, u_n)) * dS + inner(inv_eta * v_n, u_n) * ds
+a += plus(inner(vhat, uhat)) * dS + inner(inv_eta * vhat, uhat) * ds
 
 if args.damp:
     # damping diagonal terms
     du = u_n - uhat
     dv = v_n - vhat
-    a -= both(inner(dv, du)) * dS + inner(dv, eta * du) * ds
+    a += both(inner(dv, du)) * dS + inner(dv, eta * du) * ds
 
-f = Constant([1,0])
-F = inner(f, q)*dx
+f = Constant(1 if complex_mode else [1, 0])
+F = inner(q, f) * dx
 bcs = []
 
 w = Function(W, name="solution")
-for wsub, name in zip(w.subfunctions, ("u", "p")):
+for wsub, name in zip(w.subfunctions, ("u", "p", "traces")):
     wsub.rename(name)
 
 factor = {
@@ -78,6 +107,7 @@ factor = {
 cparams = {
     "ksp_monitor": None,
     "ksp_type": "gmres",
+    "ksp_view_eigenvalues": None,
     "pc_type": "python",
     "pc_python_type": "firedrake.ASMStarPC",
     "pc_star_construct_dim": 0,
@@ -92,10 +122,8 @@ sparams = {
     "pc_sc_eliminate_fields": "0,1",
     "condensed_field": cparams,
 }
-#solve(a==F, w, bcs=bcs)
 problem = LinearVariationalProblem(a, F, w)
 solver = LinearVariationalSolver(problem, solver_parameters=sparams)
-
 
 print = PETSc.Sys.Print
 print("dim(W) = ", W.dim(), tuple(V.dim() for V in W))
@@ -104,10 +132,8 @@ solver.solve()
 
 File("output/bad_cop_stab.pvd").write(*w.subfunctions[:2])
 
-A = assemble(a)
-
-
 #from firedrake.preconditioners.hypre_ams import chop
+#A = assemble(a)
 #Amat = A.petscmat
 #Amat = Amat.convert("seqaij")
 #Bmat = chop(Amat)
