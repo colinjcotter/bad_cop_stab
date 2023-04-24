a5bc7d70aacb54d778400256c6838ab750b6c8d2
Pablo Brubeck <brubeck@protonmail.com>
2023-04-24 13:30:18 +0100

    optionally eliminate the pressure

--- bad_cop_stab_alt.py
+++ bad_cop_stab_alt.py
@@ -8,6 +8,8 @@ parser = argparse.ArgumentParser(description='Stabilised conforming Hdiv badcop'
 parser.add_argument('--kappa', type=float, default=1.0, help='kappa')
 parser.add_argument('--eta', type=float, default=1.0, help='eta')
 parser.add_argument('--cr', action="store_true", default=False, help='use Crouzeix--Raviart instead of HDiv Trace')
+parser.add_argument('--no-stab', dest="stab", action="store_false", default=True, help='do not use stablization')
+parser.add_argument('--no-pressure', dest="pressure", action="store_false", default=True, help='eliminate pressure')
 parser.add_argument('--complex', action="store_true", default=False, help='use complex mode')
 parser.add_argument('--quadrilateral', action="store_true", default=False, help='use tensor-product cells')
 parser.add_argument('--nx', type=int, default=10, help='elements along each side')
@@ -15,6 +17,8 @@ parser.add_argument('--refine', type=int, default=0, help='level of refinement')
 parser.add_argument('--degree', type=int, default=1, help='degree')
 args, _ = parser.parse_known_args()
 
+stab = args.stab
+pressure = args.pressure
 complex_mode = args.complex
 dist_params = {"overlap_type": (DistributedMeshOverlapType.VERTEX, 1),}
 
@@ -23,12 +27,21 @@ mesh = UnitSquareMesh(nx, nx, quadrilateral=args.quadrilateral, distribution_par
 mh = MeshHierarchy(mesh, args.refine)
 mesh = mh[-1]
 
+eta = Constant(args.eta, domain=mesh)
+kappa = Constant(args.kappa, domain=mesh)
+
 degree = args.degree
 cell = mesh.ufl_cell()
 is_simplex = cell.is_simplex()
+if is_simplex:
+    rt_family = "RT"
+    dg_family = "DG"
+else:
+    rt_family = "RTCF" if cell.topological_dimension() == 2 else "NCF"
+    dg_family = "DQ"
 
-RT = FiniteElement("RT" if is_simplex else "RTCF", cell=cell, degree=degree)
-DG = FiniteElement("DG" if is_simplex else "DQ", cell=cell, degree=degree-1)
+RT = FiniteElement(rt_family, cell=cell, degree=degree)
+DG = FiniteElement(dg_family, cell=cell, degree=degree-1)
 if args.cr:
     T = FiniteElement("CR", cell=cell, degree=degree)
 else:
@@ -36,27 +49,52 @@ else:
 
 BrokenRT = BrokenElement(RT)
 if complex_mode:
-    j = Constant(1j)
-    T = VectorElement(T, dim=2)
+    j = Constant(1j, domain=mesh)
+    if stab:
+        T = VectorElement(T, dim=2)
 else:
-    j = Constant([[0, -1], [1, 0]])
+    j = Constant([[0, -1], [1, 0]], domain=mesh)
     BrokenRT = VectorElement(BrokenRT, dim=2)
     DG = VectorElement(DG, dim=2)
-    T = TensorElement(T, shape=(2, 2))
+    if stab:
+        T = TensorElement(T, shape=(2, 2))
+    else:
+        T = VectorElement(T, dim=2)
 
-W = FunctionSpace(mesh, MixedElement([BrokenRT, DG, T]))
-u, p, up_hat = TrialFunctions(W)
-v, q, vq_hat = TestFunctions(W)
-uhat, phat = up_hat[0], up_hat[1]
-vhat, qhat = vq_hat[0], vq_hat[1]
+if pressure:
+    elements = (BrokenRT, DG, T)
+else:
+    elements = (BrokenRT, T)
+element = MixedElement(elements)
+W = FunctionSpace(mesh, element)
+w = Function(W, name="solution")
+
+names = ("u", "p", "traces")
+if len(W) == 2:
+    names = names[::2]
+for wsub, name in zip(w.subfunctions, names):
+    wsub.rename(name)
+
+if len(W) == 3:
+    u, p, up_hat = TrialFunctions(W)
+    v, q, vq_hat = TestFunctions(W)
+else:
+    u, up_hat = TrialFunctions(W)
+    v, vq_hat = TestFunctions(W)
+    # these are off by a factor of i
+    p = div(u) / kappa
+    q = div(v) / kappa
+
+if stab:
+    uhat, phat = up_hat[0], up_hat[1]
+    vhat, qhat = vq_hat[0], vq_hat[1]
+else:
+    phat = up_hat
+    qhat = vq_hat
 
 n = FacetNormal(mesh)
 u_n = dot(u, n)
 v_n = dot(v, n)
-
-eta = Constant(args.eta)
-kappa = Constant(args.kappa)
-ikappa = kappa * j
 
 def minus(expr):
     return expr('-')
@@ -67,41 +105,40 @@ def plus(expr):
 def both(expr):
     return expr('+') + expr('-')
 
-dx0 = dx(degree=2*degree+1)
-dx1 = dx(degree=2*degree-1)
-ds = ds(degree=2*degree-1)
-dS = dS(degree=2*degree-1)
+dx0 = dx(degree=2*degree+1, domain=mesh)
+dx1 = dx(degree=2*degree-1, domain=mesh)
+ds1 = ds(degree=2*degree-1, domain=mesh)
+dS1 = dS(degree=2*degree-1, domain=mesh)
 
+ikappa = kappa * j
 # usual diagonal terms
 a = (- inner(v, ikappa * u) * dx0
      + inner(q, ikappa * p) * dx1
-     - inner(qhat, eta * phat) * ds)
+     - inner(qhat, eta * phat) * ds1)
 
-# usual divergence off-diagonal terms
-a += (inner(q, div(u)) + inner(div(v), p)) * dx1
+if len(W) == 3:
+    # usual divergence off-diagonal terms
+    a += (inner(q, div(u)) + inner(div(v), p)) * dx1
 
 # usual constraint off-diagonal terms
-c = lambda qhat, u_n: both(inner(qhat, u_n)) * dS + inner(qhat, u_n) * ds
-a -= c(qhat, u_n) + c(phat, v_n)
+a -= both(inner(qhat, u_n)) * dS1 + inner(qhat, u_n) * ds1
+a -= both(inner(phat, v_n)) * dS1 + inner(phat, v_n) * ds1
 
 # stabilization terms
-a += ((minus(inner(v_n + vhat, u_n + uhat))
-       + plus(inner(v_n - vhat, u_n - uhat))) * dS
-      + inner(v_n - vhat, (1/eta) * (u_n - uhat)) * ds)
+if stab:
+    a += ((minus(inner(v_n + vhat, u_n + uhat))
+           +plus(inner(v_n - vhat, u_n - uhat))) * dS1
+          + inner(v_n - vhat, (1/eta) * (u_n - uhat)) * ds1)
 
 # Right-hand side
-f = Constant(1 if complex_mode else [1, 0])
-F = inner(q, f) * dx
+f = Constant(1 if complex_mode else [1, 0], domain=mesh)
+F = inner(q, f) * dx1
 bcs = []
-
-w = Function(W, name="solution")
-for wsub, name in zip(w.subfunctions, ("u", "p", "traces")):
-    wsub.rename(name)
 
 gmg = lambda coarse, levels: {
     "pc_type": "mg",
-    "mg_levels": levels,
     "mg_coarse": coarse,
+    "mg_levels": levels,
 }
 
 factor = lambda solver="petsc": {
@@ -129,13 +166,15 @@ sparams = {
     "pc_type": "python",
     "mat_type": "matfree",
     "pc_python_type": "firedrake.SCPC",
-    "pc_sc_eliminate_fields": "0,1",
+    "pc_sc_eliminate_fields": ",".join(map(str, range(len(W)-1))),
     "condensed_field": cparams,
 }
+
 problem = LinearVariationalProblem(a, F, w)
 solver = LinearVariationalSolver(problem, solver_parameters=sparams, options_prefix="")
 
-print("dim(W) = ", W.dim(), tuple(V.dim() for V in W))
+print("dim(W) =", W.dim(), tuple(V.dim() for V in W))
 print("kappa =", float(kappa))
 solver.solve()
-File("output/bad_cop_stab.pvd").write(*w.subfunctions[:2])
+
+File("output/bad_cop_stab.pvd").write(*w.subfunctions[:-1])
